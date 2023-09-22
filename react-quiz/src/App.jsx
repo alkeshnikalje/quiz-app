328400c18800d9d0077ffc3299db0bb3e6130f11
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-09-22 22:58:20 +0530

    added loader, error and startScreen components

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,11 +1,44 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+
+const initialState = {
+  questions: [],
+  status: "Loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "Ready" };
+    case "dataFailed":
+      return { ...state, status: "Error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
 
   return (
     <div className="app">
       <Header />
+      <Main>
+        {status === "Loading" && <Loader />}
+        {status === "Ready" && <StartScreen />}
+        {status === "Error" && <Error />}
+      </Main>
     </div>
   );
 }

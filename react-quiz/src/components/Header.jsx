43e7be5c2d23c75db68bc30d97c22fee8947a95a
Alkesh Nikalje <alkeshnikalje@gmail.com>
2023-09-22 16:22:55 +0530

    added header component

---
+++ react-quiz/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="app-header">
+      <img src="reactlogo.png" alt="React logo" />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
